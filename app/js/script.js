0dcd0c494257ba1ac8ebb224ee8e66b0e53ab468
meloman1001 <meloman1001@mail.ru>
2020-12-11 19:45:28 +0300

    popUp Заказать c проверкой адреса

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,11 @@
-@@include('jquery-3.5.1.min.js')
-@@include('bootstrap.js')
-@@include('slick.js')
-@@include('jquery.mask.js')
-@@include('jquery.validate.js')
+@@include( 'jquery-3.5.1.min.js' )
+@@include( 'bootstrap.js' )
+@@include( 'slick.js' )
+@@include( 'jquery.mask.js' )
+@@include( 'jquery.validate.js' )
 
-$(document).ready(function () {
-   $('.tariffs .slider').slick({
+$( document ).ready( function () {
+   $( '.tariffs .slider' ).slick( {
       infinite: false,
       slidesToShow: 4,
       slidesToScroll: 1,
@@ -39,9 +39,9 @@ $(document).ready(function () {
             }
          }
       ]
-   });
+   } );
 
-   $('.equipment__slider').slick({
+   $( '.equipment__slider' ).slick( {
       infinite: false,
       slidesToShow: 1,
       slidesToScroll: 1,
@@ -54,15 +54,15 @@ $(document).ready(function () {
             }
          }
       ]
-   });
+   } );
 
-   $('input[name="phone"]').mask('+7 (000) 000 - 00 - 00', {placeholder: "+7 (   )     -    -   "});
+   $( 'input[name="phone"]' ).mask( '+7 (000) 000 - 00 - 00', {placeholder: "+7 (   )     -    -   "} );
 
-   $('#orderForm').validate({
+   $( '#orderForm' ).validate( {
       rules: {
          phone: {
             required: true,
-            minlength: 22
+            minlength: 22,
          },
          name: {
             required: true
@@ -77,18 +77,66 @@ $(document).ready(function () {
             required: 'Заполните поле!'
          }
       }
-   })
+   } )
 
-})
+   // $( '#orderOpportunity' ).validate( {
+   //    rules: {
+   //       address: {
+   //          required: true,
+   //       }
+   //    },
+   //    messages: {
+   //       address: {
+   //          required: ' '
+   //       }
+   //    },
+   //    success: function (label) {
+   //       label.addClass( "valid" ).text( "Ура! Вы в зоне покрытия!" )
+   //       $('#btnCheck').removeAttr('disabled').text('Далее')
+   //    },
+   //    submitHandler: function() {
+   //       alert()
+   //    },
+   // } )
+
+} )
 
 function toPlug() { // click "подключить"
-   $('html,body').animate({
+   $( 'html,body' ).animate( {
       scrollTop:
-         $('.tariffs').offset().top + "px"
+         $( '.tariffs' ).offset().top + "px"
    }, {
       duration: 300
-   })
+   } )
 }
+
+function fixDate() { // клик "Выбрать дату"
+   $('.order-opportunity').addClass('d-flex')
+   $('.requisition').addClass('d-none')
+}
+
+function back() {
+   $('.order-opportunity').removeClass('d-flex')
+   $('.requisition').removeClass('d-none')
+}
+
+function valid(e) {
+   if(e.target.value === 'test') {
+      console.log(e.path[0])
+      e.path[0].classList.add('valid')
+      e.path[0].classList.remove('error')
+      $('#btnCheck').attr('disabled', false).text('Далее')
+   } else if (e.target.value === '') {
+      e.path[0].classList.remove('valid')
+      e.path[0].classList.remove('error')
+      $('#btnCheck').attr('disabled', true).text('Проверить')
+   } else {
+      e.path[0].classList.remove('valid')
+      e.path[0].classList.add('error')
+      $('#btnCheck').attr('disabled', false).text('Подробнее')
+   }
+}
+
 
 
 
