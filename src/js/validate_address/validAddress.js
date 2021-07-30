ef06e3cf8fcb6aa3ec825bf7180903e7973dc606
Pavel.A.Ivanov <Pavel.A.Ivanov@wifire.ru>
2021-07-30 11:16:26 +0300

    fix / submit label
    Подсказка при клике на кнопку "проверить"

--- src/js/validate_address/validAddress.js
+++ src/js/validate_address/validAddress.js
@@ -1,15 +1,20 @@
 import prop from "./prop";
 import {showResultMainCheck} from "./showResultMainCheck";
+import {showHideLabel} from "./showHideLabel";
 
 export const validAddress = (data) => {
    prop.setAddress = {}
+
    if (data.result === 1) {
       showResultMainCheck( 'show', 'hide', '.success-check b' )
       gtag( 'event', 'click', {'event_category': 'EventFMC', 'event_label': 'check_address_success'} )
    }
+
    if (data.result === 0) {
       calcRadio.sum( '#unite', 0 )
       showResultMainCheck( 'hide', 'show', '.unite-address__offer b' )
       gtag( 'event', 'click', {'event_category': 'EventFMC', 'event_label': 'check_address_fail'} )
    }
+
+   if ($( prop.input ).val() && !prop.flagValid) showHideLabel('show')
 }
