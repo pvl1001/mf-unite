3ecd8766f5273b45da94a618828f48b528fbe672
pavel.a.ivanov <pavel.a.ivanov@wifire.ru>
2021-08-09 15:13:50 +0300

    fix / webpack.config.js
    получить вложенные папки
    подключение json файлов

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const
+   fs = require( 'fs' ),
    path = require( 'path' ),
    webpack = require( "webpack" ),
    HTMLWebpackPlugin = require( 'html-webpack-plugin' ),
@@ -10,18 +11,37 @@ const
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
 
+// получить вложенные папки
+const getFiles = (dir, files_) => {
+
+   files_ = files_ || []
+   const files = fs.readdirSync( dir )
+   for (let i in files) {
+      const name = dir + '/' + files[i]
+      if (fs.statSync( name ).isDirectory()) {
+         getFiles( name, files_ )
+      } else {
+         const splitName = name.split( '/' )
+         splitName.splice( -1, 1 )
+         const dirFolder = splitName.join( '/' )
+         files_.push( path.win32.join( __dirname, dirFolder ) )
+      }
+   }
+   return Array.from( new Set( [...files_] ) )
+}
+
 module.exports = {
    mode: 'development',
-   context: path.resolve( __dirname, 'src' ),
    entry: { // точка входы
       main: './main.js'
    },
+   context: path.win32.resolve( __dirname, 'src' ),
    output: { // точка вывода
       filename: 'bundle.js',
-      path: path.resolve( __dirname, 'dist' )
+      path: path.win32.resolve( __dirname, 'dist' )
    },
    devServer: { // сервер
-      contentBase: path.join( __dirname, 'dist' ),
+      contentBase: path.win32.join( __dirname, 'dist' ),
       port: 3000,
       open: false,
       overlay: true,
@@ -30,7 +50,7 @@ module.exports = {
    resolve: {
       extensions: ['.css', '.scss', '.js', '.json'],
       alias: {
-         '@': path.resolve( __dirname, 'src' ) // корневая папка
+         '@': path.win32.resolve( __dirname, 'src' ) // корневая папка
       }
    },
    plugins: [
@@ -43,13 +63,13 @@ module.exports = {
          filename: 'index.html', // html на выходе
          template: './index.hbs', // шаблон
          templateParameters() { // данные шаблона
-            const tariffs = require( './src/json/tariffs' )
-            const performance = require( './src/json/performance' )
-            const info = require( './src/json/info' )
-            const cities = require( './src/json/cities' )
-            const unite = require( './src/json/unite' )
-
-            return {tariffs, performance, info, cities, unite}
+            const jsonFiles = fs.readdirSync( 'src/json' )
+            const jsonProps = {}
+            jsonFiles.forEach( file => {
+               const fileName = file.split( '.' )[0]
+               jsonProps[fileName] = require( './src/json/' + file )
+            } )
+            return jsonProps
          },
          minify: isProd, // минимизировать html
       } ),
@@ -64,13 +84,8 @@ module.exports = {
             test: /\.hbs$/,
             loader: "handlebars-loader",
             options: {
-               helperDirs: [
-                  path.join( __dirname, 'src', 'helpers' )
-               ],
-               partialDirs: [
-                  path.join( __dirname, 'src', 'components' ),
-                  path.join( __dirname, 'src', 'components', 'modals' ),
-               ]
+               helperDirs: getFiles( './src/helpers' ),
+               partialDirs: getFiles( './src/components' )
             }
          },
          {
